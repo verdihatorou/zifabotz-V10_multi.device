af8c3ca1f621debf88eb567ca811c12b1fe43ae8
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-22 12:22:06 +0700

    Update inforozi.js

--- plugins/inforozi.js
+++ plugins/inforozi.js
@@ -2,13 +2,13 @@ let handler  = async (m, { conn, usedPrefix }) => {
 conn.reply(m.chat, `
 ╭═══════════════════════
 ║╭──❉ 〔 INFO OWNER 〕 ❉────── 
-║│➸ ```NAMA``` : ROZI
-║│➸ ```UMUR``` : 15thn
-║│➸ ```ASAL``` : PONTIANAK
+║│➸ ```NAMA``` : verdi
+║│➸ ```UMUR``` : 14thn
+║│➸ ```ASAL``` : -
 ║│➸ ```OFFICIAL GRUP``` :
 https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv
-║│➸ ```ISTAGRAM``` : http://instagram.com/zalfapontianak
-║│➸ ```WHATSAPP``` : http://wa.me/6285828764046
+║│➸ ```ISTAGRAM``` : http://instagram.com/VERDI_ZZ
+║│➸ ```WHATSAPP``` : http://wa.me/6285706735450
 ╰────────❉
 `.trim(), m)
 }
