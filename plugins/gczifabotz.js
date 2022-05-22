d6dc6a92245e7c72d5a434a4cf2a31a633e5f067
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-22 12:01:35 +0700

    Update gczifabotz.js

--- plugins/gczifabotz.js
+++ plugins/gczifabotz.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.botwm
 let str = `
-*Group zifabotz1 📮: https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv*
+*Group verdibotz1 📮: wa.me//+6285706735450*
 `.trim()
 conn.sendButton(m.chat, str, `${botol}`, `⋮☰ Menu`, `.menu`, m)
 }
